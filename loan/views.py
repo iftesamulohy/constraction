484e1606e92d23451ae2d5dffa24265b187ffe59
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2023-09-16 13:01:06 +0600

    tokenised

--- loan/views.py
+++ loan/views.py
@@ -40,6 +40,25 @@ class AllLoanBeneficaries(viewsets.ModelViewSet):
         item.is_deleted = True
         item.save()
         return Response({"message": "Loan Beneficaries deleted. But you can recover your data"})
+    def list(self, request, *args, **kwargs):
+        # Get the paginated queryset
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            # Serialize the paginated data
+            serializer = self.get_serializer(page, many=True)
+
+            # Return the paginated response
+            token = encode_jwt({"data":serializer.data})
+            return self.get_paginated_response({"token":token})
+
+        # If there is no pagination, serialize the entire queryset
+        serializer = self.get_serializer(queryset, many=True)
+
+        # Return the serialized data without pagination
+        token = encode_jwt({"data":serializer.data})
+        return Response({"token":token})
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         phone_numbers = serializer.initial_data['phone_number']
@@ -78,25 +97,6 @@ class AllLoanTransactions(viewsets.ModelViewSet):
     filterset_class = LoanTransactionsFilter  # Use the custom filter class
     def get_queryset(self):
         return LoanTransactions.objects.filter(is_deleted=False)
-    def list(self, request, *args, **kwargs):
-        # Get the paginated queryset
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-
-        if page is not None:
-            # Serialize the paginated data
-            serializer = self.get_serializer(page, many=True)
-
-            # Return the paginated response
-            token = encode_jwt({"data":serializer.data})
-            return self.get_paginated_response({"token":token})
-
-        # If there is no pagination, serialize the entire queryset
-        serializer = self.get_serializer(queryset, many=True)
-
-        # Return the serialized data without pagination
-        token = encode_jwt({"data":serializer.data})
-        return Response({"token":token})
     @action(detail=True, methods=['post'])
     def soft_delete(self, request, pk=None):
         item = self.get_object()
